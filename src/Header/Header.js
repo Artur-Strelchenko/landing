d3861690605dd505b5cfc8e0bd31e25474118fe6
Artur-Strelchenko <artur.strelchenko98@gmail.com>
2025-12-27 16:39:55 +0200

    feat: add styles for header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,33 +3,36 @@ import "./header.css";
 export const Header = () => {
   return (
     <div className="container-header">
-      <img src="../img/logo.png" alt="asd" />
+      <a href="/">
+        <img src="../img/logo.png" alt="logo" />
+      </a>
 
       <div className="header-nav">
         <a href="/" className="link">
           Products
-          <img src="../img/Vector.png" alt= "asd" />
+          <img src="../img/Vector.png" alt="vector" className="icon-arrow" />
         </a>
         <a href="/" className="link">
           Solutions
-          <img src="../img/Vector.png" alt= "asd" />
+          <img src="../img/Vector.png" alt="vector" className="icon-arrow" />
         </a>
         <a href="/" className="link">
           Resources
-          <img src="../img/Vector.png" alt= "asd" />
+          <img src="../img/Vector.png" alt="vector" className="icon-arrow" />
         </a>
         <a href="/" className="link">
           Pricing
-          <img src="../img/Vector.png" alt= "asd" />
+          <img src="../img/Vector.png" alt="vector" className="icon-arrow" />
         </a>
       </div>
 
       <div className="header-actions">
-        <button className="header-button-yellow">Login</button> 
+        <button className="header-button-yellow">Login</button>
         <button className="header-button-blue">
           Try Whitepace free
-          <img src="../img/Icon.png" alt="asd"/>
-          </button>
+          <img src="../img/Icon.png" alt="arrow" />
+        </button>
+        <div className="burger-menu">=</div>
       </div>
     </div>
   );
