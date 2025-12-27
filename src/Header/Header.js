897bec076b9de800e20d77c78b046d7081a5b4c5
Artur-Strelchenko <borysdk@gmail.com>
2025-12-27 15:21:11 +0200

    https://github.com/Artur-Strelchenko/landing.git

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,21 +8,28 @@ export const Header = () => {
       <div className="header-nav">
         <a href="/" className="link">
           Products
+          <img src="../img/Vector.png" alt= "asd" />
         </a>
         <a href="/" className="link">
           Solutions
+          <img src="../img/Vector.png" alt= "asd" />
         </a>
         <a href="/" className="link">
           Resources
+          <img src="../img/Vector.png" alt= "asd" />
         </a>
         <a href="/" className="link">
           Pricing
+          <img src="../img/Vector.png" alt= "asd" />
         </a>
       </div>
 
-      <div>
-        <button className="header-button-yellow">Login</button>
-        <button>Try Whitepace free</button>
+      <div className="header-actions">
+        <button className="header-button-yellow">Login</button> 
+        <button className="header-button-blue">
+          Try Whitepace free
+          <img src="../img/Icon.png" alt="asd"/>
+          </button>
       </div>
     </div>
   );
